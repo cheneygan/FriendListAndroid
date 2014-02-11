eaaee60f7b1c35cc25ce18aae089959329267ecb
Kento Sasamoto <knote0721@gmail.com>
2014-02-11 12:04:46 +0900

    add side menu

--- src/com/example/friendlist/MainActivity.java
+++ src/com/example/friendlist/MainActivity.java
@@ -4,24 +4,41 @@ import com.facebook.android.DialogError;
 import com.facebook.android.Facebook;
 import com.facebook.android.Facebook.DialogListener;
 import com.facebook.android.FacebookError;
-
+import com.navdrawer.SimpleSideDrawer;
 import android.os.Bundle;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
 
 public class MainActivity extends Activity {
 	
 	 @SuppressWarnings("deprecation")
 	Facebook facebook = new Facebook("755345174478792");
+	SimpleSideDrawer sideMenu;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
+		
 		
 		Facebook();
 		
+		//SideMenu関連
+		sideMenu = new SimpleSideDrawer(this);
+		sideMenu.setRightBehindContentView(R.layout.side_menu);		
+        findViewById(R.id.side_menu).setOnClickListener(new OnClickListener() {
+            @Override 
+            public void onClick(View v) {
+            	sideMenu.toggleRightDrawer();
+            }
+        });
+
 	}
 
 	@Override
