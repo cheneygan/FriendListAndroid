d63534a69cc14afc639396d461548834ceb30c21
Kento Sasamoto <knote0721@gmail.com>
2014-02-14 01:51:52 +0900

    success genymotion

--- src/com/example/friendlist/MainActivity.java
+++ src/com/example/friendlist/MainActivity.java
@@ -1,15 +1,15 @@
 package com.example.friendlist;
 
 import org.json.JSONArray;
-import org.json.JSONObject;
 
 import com.android.volley.Request.Method;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.Response.ErrorListener;
 import com.android.volley.Response.Listener;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.facebook.android.DialogError;
 import com.facebook.android.Facebook;
@@ -30,6 +30,8 @@ public class MainActivity extends Activity {
 	Facebook facebook = new Facebook("755345174478792");
 	SimpleSideDrawer sideMenu;
 	RequestQueue rQueue;
+	String url = "http://192.168.56.1:3000/api/getgroups?user_id=1"; //samole URL
+	String loginurl = "http://192.168.56.1:3000/login/test?token="; //Login URL
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -50,29 +52,8 @@ public class MainActivity extends Activity {
             }
         });
 
-		String url = "http://10.0.2.2:3000/api/getgroups?user_id=1";
 		rQueue = Volley.newRequestQueue(this);
-		 JsonArrayRequest request = new  JsonArrayRequest(url,
-				new Listener<JSONArray>(){
-					@Override
-					public void onResponse(JSONArray result) {
-						// TODO Auto-generated method stub
-						Log.e("成功", result.toString());
-						
-					}
-				}, 
-				new Response.ErrorListener() {
-		
-					@Override
-					public void onErrorResponse(VolleyError result) {
-						// TODO Auto-generated method stub
-						Log.e("失敗", result.toString());
-						
-					}
-				});
-		rQueue.add(request);
-
-
+		SampleAPI();
 	}
 
 	@Override
@@ -84,15 +65,15 @@ public class MainActivity extends Activity {
 	//Facebook Token取得メソッド
 	@SuppressWarnings("deprecation")
 	public void Facebook() {
-		facebook.authorize(this, new String[] { "email", "offline_access", "publish_checkins" }, 
+		facebook.authorize(this, new String[] { "user_events", "user_groups", "user_friends","read_stream" }, 
 				new DialogListener(){
 			@Override
 			public void onComplete(Bundle values) {
 				// TODO Auto-generated method stub
-				Log.e("成功", "成功");
-				
+				Log.e("Facebook Token", facebook.getAccessToken());
+				loginurl = loginurl+facebook.getAccessToken();
+				loginRequest();
 			}
-
 			@Override
 			public void onFacebookError(FacebookError e) {
 				// TODO Auto-generated method stub
@@ -113,5 +94,46 @@ public class MainActivity extends Activity {
 			
 		});
 	}
-
+	public void SampleAPI() {
+		JsonArrayRequest request = new  JsonArrayRequest(url,
+				new Listener<JSONArray>(){
+					@Override
+					public void onResponse(JSONArray result) {
+						// TODO Auto-generated method stub
+						Log.e("成功", result.toString());
+						
+					}
+				}, 
+				new Response.ErrorListener() {
+		
+					@Override
+					public void onErrorResponse(VolleyError result) {
+						// TODO Auto-generated method stub
+						Log.e("失敗", result.toString());
+						
+					}
+				});
+		rQueue.add(request);		
+	}
+	public void loginRequest() {
+		
+		Log.e("LoginRequest", "呼ばれたよ！");
+		StringRequest stringRequest = new StringRequest(Method.GET, loginurl,
+				new Listener<String>(){
+			@Override
+			public void onResponse(String result) {
+				// TODO Auto-generated method stub
+				Log.e("成功", result.toString());
+			}
+		}, 
+		new Response.ErrorListener() {
+			@Override
+			public void onErrorResponse(VolleyError result) {
+				// TODO Auto-generated method stub
+				Log.e("失敗", result.toString());
+				
+			}
+		});
+		rQueue.add(stringRequest);		
+	}
 }
